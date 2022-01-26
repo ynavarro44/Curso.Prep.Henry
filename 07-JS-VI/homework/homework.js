b6f3f4e4a70050a6ceb717f60d3be2a217477200
Yina <yinanavarrov@gmail.com>
2022-01-26 17:36:42 -0500

    Homework07 by Yina termnado

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -58,13 +58,10 @@ function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
-  var nuevoArray = [];
-  for (let i = 0; i < array.length; i++){
-    if (array[i][0] === "a"){
-      nuevoArray.push(array[i]);
-    }
-  }
-  return nuevoArray;
+  var resultado = array.filter(function(palabra){
+    return palabra[0] === "a" 
+  })
+  return resultado
 }
 
 // No modificar nada debajo de esta línea
